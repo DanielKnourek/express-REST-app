82801167beb5bdd0a498ccd52680381d20ecf630
Daniel Kňourek <knourek.daniel@gmail.com>
2024-04-05 07:44:43 +0000

    feat: workspace init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+import customer from './utils/customer';
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+  customer.createCustomer({display_name: 'James Bond'});
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
