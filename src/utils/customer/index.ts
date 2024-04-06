7e537c6bd07f34b73341637b2eb810df807cfcd3
Daniel Kňourek <knourek.daniel@gmail.com>
2024-04-06 07:42:03 +0000

    refactor: variable renaming

--- src/utils/customer/index.ts
+++ src/utils/customer/index.ts
@@ -4,15 +4,15 @@ import { ResponseData } from '@/utils/index';
 import { z } from 'zod';
 import { UUID, randomUUID } from 'crypto';
 
-const createCustomer = async (customer: newCustomer): Promise<ResponseData<customer>> => {
+const createCustomer = async (customer_data: newCustomer): Promise<ResponseData<customer>> => {
     const sql_querry = /*sql*/`INSERT INTO customer(uuid, display_name) VALUES (UUID_TO_BIN(?), ?)`;
     const uuid = randomUUID();
 
     return getConnection({
-        message: `Creating customer {${customer.display_name} and uuid: {${uuid}}`
+        message: `Creating customer {${customer_data.display_name} and uuid: {${uuid}}`
     })
         .then((connection) => {
-            return connection.execute(sql_querry, [uuid, customer.display_name])
+            return connection.execute(sql_querry, [uuid, customer_data.display_name])
         })
         .then(([rows]) => {
             if ((rows as any).affectedRows != 1) { // TODO: cast to any
@@ -24,7 +24,7 @@ const createCustomer = async (customer: newCustomer): Promise<ResponseData<custo
                 success: true,
                 result: {
                     uuid: uuid,
-                    display_name: customer.display_name,
+                    display_name: customer_data.display_name,
                 }
             } as ResponseData<customer>
         })
