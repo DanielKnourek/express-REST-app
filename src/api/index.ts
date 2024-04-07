24d2e5e853000a2c9049e8fa1b10565e3632e55d
DanielKnourek <48413610+DanielKnourek@users.noreply.github.com>
2024-04-07 18:15:50 +0200

    Dev/feature authorization (#3)
    * feat: return acces_token on listing your users
    
    * feat: adding authorization for user api
    
    * docs: adding docs
    
    ---------
    
    Co-authored-by: Daniel Kňourek <knourek.daniel@gmail.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import customer from '@/api/customer';
 import user from '@/api/user';
+import user_lib from '@/utils/user';
+import { User, userBarerTokenSchema } from '@/utils/user/userSchema';
 
 const registerApiRoutes = (app: express.Application) => {
     const api = getSubrouter(app, '/api')
-        .use(express.json());
+        .use(express.json())
+        .use(require_access_token);
 
     api.get('/', (req, res) => {
         res.send('Welcome to the API');
@@ -17,10 +20,45 @@ const registerApiRoutes = (app: express.Application) => {
 
 };
 
+/**
+ * provides subrouter for a given path and router
+ * @param app root router
+ * @param path path for subrouter
+ * @returns router with path "${router.path}${path}"
+ */
 const getSubrouter = (app: express.Router, path: string) => {
     const subrouter = express.Router({mergeParams: true});
     app.use(path, subrouter);
     return subrouter;
 }
+
+declare global {
+    namespace Express {
+        interface Request {
+            caller?: User;
+        }
+    }
+}
+
+/**
+ * middleware for parsing and matching access_token to a user,
+ * then user data are added to request object
+ */
+const require_access_token: express.RequestHandler = async (req, res, next) => {
+    const req_caller = userBarerTokenSchema.safeParse(req.headers);
+    if (!req_caller.success) {
+        res.status(400).send('Missing or invalid access token');
+        return;
+    }
+
+    const caller = await user_lib.getBy({ access_token: req_caller.data.access_token })
+    if (!caller.success) {
+        res.status(401).send('Could find user while authenticating');
+        return;
+    }
+
+    req.caller = caller.result;
+    next();
+    }
 
 export { registerApiRoutes };
