f284e6d2f6108193b2659c13ec6fcd929745970e
Daniel Kňourek <knourek.daniel@gmail.com>
2024-04-07 08:29:33 +0000

    feat: system user, customer

--- migrations/sqls/20240405095939-system-users-up.sql
+++ migrations/sqls/20240405095939-system-users-up.sql
@@ -1,6 +1,18 @@
-INSERT INTO `user` (`uuid`, `access_key`, `username`, `full_name`, `role`)
+INSERT INTO `user` (`uuid`, `access_token`, `username`, `full_name`, `role`)
 VALUES (
     uuid_to_bin('00000000-0000-0000-0000-000000000000'),
     UNHEX('0000000000000000000000000000000000000000000000000000000000000000'),
-    'system', 'System', 1
+    'system_user', 'System user', 1
+    );
+
+INSERT INTO `customer` (`uuid`, `display_name`)
+VALUES (
+    uuid_to_bin('00000000-1111-0000-1111-000000000000'),
+    'System Customer'
+    );
+
+INSERT INTO `customer_user` (`user_fk`, `customer_fk`)
+VALUES (
+    uuid_to_bin('00000000-0000-0000-0000-000000000000'),
+    uuid_to_bin('00000000-1111-0000-1111-000000000000')
     );
